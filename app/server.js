72422f675df35e2604a29cb4bbabb1a693fe04b6
Hadi <hadi.tajallaei@defra.gov.uk>
2023-10-12 12:03:46 +0100

    Successfully instantiated graphql API via hapi

--- app/server.js
+++ app/server.js
@@ -3,41 +3,28 @@ const path = require('path')
 require('./insights').setup()
 const Hapi = require('@hapi/hapi')
 const  ApolloServer = require('@apollo/server');
-const hapiApollo = require('@as-integrations/hapi');
 const graphql =  require('@graphql-tools/load-files')
 
+const typeDefs = graphql.loadFilesSync(
+  path.join(__dirname, 'graphql', 'types'),
+  { recursive: true }
+)
+
+const resolvers = graphql.loadFilesSync(
+  path.join(__dirname, 'graphql', 'resolvers'),
+  { recursive: true }
+)
+
 const apolloServer = new ApolloServer.ApolloServer({
-  typeDefs: graphql.loadFilesSync(
-    path.join(__dirname, 'graphql', 'types'),
-    { recursive: true }
-  ),
-  resolvers: graphql.loadFilesSync(
-    path.join(__dirname, 'graphql', 'resolvers'),
-    { recursive: true }
-  ),
+  typeDefs,
+  resolvers,
 });
-
-apolloServer.start().catch(e => {
-  console.error(e.message)
-
-  process.exit(1)
-})
 
 const server = Hapi.server({
   port: process.env.PORT
 })
 
-server.register({
-  plugin: hapiApollo,
-  options: {
-    apolloServer,
-    path: '/graphql'
-  }
-}).catch(e => {
-  console.error('error registering GraphQL API middleware for hapi', e.message)
 
-  process.exit(1)
-})
 
 const routes = [].concat(
   require('./routes/healthy'),
@@ -46,4 +33,7 @@ const routes = [].concat(
 
 server.route(routes)
 
-module.exports = server
+module.exports = {
+  apolloServer,
+  server
+}
