0b822a5e9822e33d72bd864ad65ba6fab3f1145a
Hadi <hadi.tajallaei@defra.gov.uk>
2023-10-12 11:17:55 +0100

    added integration for hapi and Apollo GraphQL Server

--- app/server.js
+++ app/server.js
@@ -1,8 +1,33 @@
 require('./insights').setup()
 const Hapi = require('@hapi/hapi')
+import { ApolloServer } from '@apollo/server';
+import hapiApollo from '@as-integrations/hapi';
+
+const apolloServer = new ApolloServer({
+  typeDefs: "",
+  resolvers: null,
+});
+
+apolloServer.start().catch(e => {
+  console.error(e.message)
+
+  process.exit(1)
+})
 
 const server = Hapi.server({
   port: process.env.PORT
+})
+
+server.register({
+  plugin: hapiApollo,
+  options: {
+    apolloServer,
+    path: '/graphql'
+  }
+}).catch(e => {
+  console.error('error registering GraphQL API middleware for hapi', e.message)
+
+  process.exit(1)
 })
 
 const routes = [].concat(
