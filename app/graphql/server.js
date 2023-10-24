07a59ebd24923df52f8f9fe150e050e400de98dc
Hadi <hadi.tajallaei@defra.gov.uk>
2023-10-24 14:52:42 +0100

    exported typedefs and resolvers to be used in unit tests

--- app/graphql/server.js
+++ app/graphql/server.js
@@ -1,26 +1,25 @@
 import { dirname, join } from 'path'
-import { pathToFileURL, fileURLToPath } from 'url'
+import { fileURLToPath } from 'url'
 
 import { loadFiles } from '@graphql-tools/load-files'
 import { ApolloServer } from '@apollo/server'
 import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled'
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export const typeDefs = (async () => await loadFiles(join(__dirname, 'types'), {
+  recursive: true
+}))()
+
+export const resolvers = (async () => await loadFiles(join(__dirname, 'resolvers'), {
+  recursive: true
+}))()
+
 export async function NewApolloServer() {
-  const __dirname = dirname(fileURLToPath(import.meta.url))
-
-  const typeDefs = await loadFiles(join(__dirname, 'types'), {
-    recursive: true
-  })
-
-  const resolvers = await loadFiles(join(__dirname, 'resolvers'), {
-    recursive: true,
-    requireMethod: async path => import(pathToFileURL(path))
-  })
-
   return new ApolloServer({
-    typeDefs,
-    resolvers,
+    typeDefs: await typeDefs,
+    resolvers: await resolvers,
     plugins: [
       (() => {
         if (process.env?.NODE_ENV === 'production') {
