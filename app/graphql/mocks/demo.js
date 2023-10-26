c028c37bee7d700673706777db86cceb6acd5bfb
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2023-10-26 10:20:03 +0100

    chore: convert to named exports

--- app/graphql/mocks/demo.js
+++ app/graphql/mocks/demo.js
@@ -1,12 +1,8 @@
 import casual from 'casual'
 
-const demoMock = {
-  Demo: () => ({
-    email: casual.email,
-    firstName: casual.first_name,
-    lastName: casual.last_name,
-    password: casual.password
-  })
-}
-
-export default demoMock
+export const Demo = () => ({
+  email: casual.email,
+  firstName: casual.first_name,
+  lastName: casual.last_name,
+  password: casual.password
+})
--- app/graphql/resolvers/demo.js
+++ app/graphql/resolvers/demo.js
@@ -1,12 +1,8 @@
-const demoResolver = {
-  Query: {
-    demo: () => ({
-      email: 'resolver email',
-      firstName: 'resolver firstName',
-      lastName: 'resolver lastName',
-      password: 'resolver password'
-    })
-  }
+export const Query = {
+  demo: () => ({
+    email: 'resolver email',
+    firstName: 'resolver firstName',
+    lastName: 'resolver lastName',
+    password: 'resolver password'
+  })
 }
-
-export default demoResolver
