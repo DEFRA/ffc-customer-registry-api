2e9717cc291deb5e21746fd4bf37f624548c1707
jackmccabe-elsewhen <66681105+j-mccabe@users.noreply.github.com>
2023-11-08 16:19:38 +0000

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,11 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
   overrides: [
     {
       files: ['*.gql'],
