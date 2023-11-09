92335dca0381691e333fa49f82f2ea70b85fb4a3
Hadi <hadi.tajallaei@defra.gov.uk>
2023-11-09 09:30:13 +0000

    removed unwanted

--- app/graphql/mocks/business.js
+++
@@ -1,7 +0,0 @@
-import { faker } from '@faker-js/faker/locale/en_GB'
-
-export const Business = () => {
-  return {
-    businessId: faker.string.alphanumeric({ length: 10, casing: 'upper' })
-  }
-}
