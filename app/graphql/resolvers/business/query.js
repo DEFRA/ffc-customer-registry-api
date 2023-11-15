437aae218e4a3cdd526fea6350b5de6ca705d31f
Dominic Taylor <dom.taylor7@gmail.com>
2023-11-15 10:35:55 +0000

    name business transformer export

--- app/graphql/resolvers/business/query.js
+++ app/graphql/resolvers/business/query.js
@@ -1,5 +1,5 @@
 import RuralPaymentsPortal from '../../../services/rural-payments-portal.js'
-import RuralPaymentsPortalBusinessTransformer from '../../../transformers/rural-payments-portal/business.js'
+import { transformOrganisationToBusiness } from '../../../transformers/rural-payments-portal/business.js'
 import { Land } from '../land/query.js'
 
 const ruralPaymentsPortal = new RuralPaymentsPortal()
@@ -7,7 +7,7 @@ const ruralPaymentsPortal = new RuralPaymentsPortal()
 export const Query = {
   async business (_, { id }) {
     const response = await ruralPaymentsPortal.getOrganisationBySBI(id)
-    return RuralPaymentsPortalBusinessTransformer(response)
+    return transformOrganisationToBusiness(response)
   }
 }
 
