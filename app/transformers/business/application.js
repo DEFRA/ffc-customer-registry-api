6c7f66d99dbead4b2f30deb072edb9181122aa2f
Hadi <hadi.tajallaei@defra.gov.uk>
2023-12-04 14:29:12 +0000

    fixed js code matching graphql changes

--- app/transformers/business/application.js
+++ app/transformers/business/application.js
@@ -6,34 +6,22 @@ export function transformOrganisationApplicationToBusinessApplications (applicat
   const result = []
   for (const response in applications) {
     result.push({
-      applicationId: response.application_id,
-      year: response.year,
-      sectorDescription: response.sector_description,
-      moduleId: response.module_id,
-      modcod: response,
-      applicationTypeDS: response.application_type_ds,
-      applicationTypeDE: response.application_type_de,
-      statusCode: response.status_code,
-      statusSubCode: response.status_sub_code,
-      statusDescription: response.status_description,
-      activeApplicationFlag: response.active_application_flag,
-      applicationMovementDate: response.application_movement_date,
-      applicationCode: response.application_code,
-      workflowContextSubCode: response.workflow_context_sub_code,
-      needsIntervention: response.needs_intervention,
-      showAccept: response.show_accept,
-      submitDate: response.submit_date,
-      status: response.status,
-      commonLandsFlag: response.common_lands_flag,
-      printsCount: response.prints_count,
-      queued: response.queued,
-      hasSSSIIntersectionC: response.has_sssi_intersection_c,
-      hasSSSIIntersectionY: response.has_sssi_intersection_y,
-      hasHeferIntersectionY: response.has_hefer_intersection_y,
-      hasBeenAGRLIV: response.has_been_AGRLIV,
-      terClosingDate: response.ter_closing_date,
-      eligibIntersections: response.eligibIntersections,
-      requiredSSSIApproval: response.required_sssi_approval
+      applicationStatus: {
+        id: response.application_id,
+        open: response.application_code,
+        status: response.status,
+        type: response.application_type_de,
+        sector: response.workflow_context_sub_code,
+        year: response.year,
+        FRN: response.has_hefer_intersection_y,
+        office: null,
+      },
+      csClaims: {
+        schemaYear: response.year,
+        type: response.application_type_ds,
+        status: response.status_sub_code,
+        lastMovement: response.application_movement_date,
+      },
     })
   }
 
