bd8fc1dc243d41f1310f87b9dd14099497b224af
Hadi <hadi.tajallaei@defra.gov.uk>
2023-12-04 08:47:33 +0000

    Added Business Applications and Person Business Type association for GraphQL

--- app/graphql/resolvers/business/query.js
+++ app/graphql/resolvers/business/query.js
@@ -4,6 +4,9 @@ export const Query = {
   async business (__, { id }, { dataSources }) {
     const response = await dataSources.ruralPaymentsPortalApi.getOrganisationBySBI(id)
     const business = transformOrganisationToBusiness(response)
+
+    // todo: create transformOrganisationApplicationToBusinessApplications() { applications: [Applications]}
+
     return {
       id,
       land: { sbi: id },
