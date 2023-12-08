7bd7ec095bde6590dfd4c77dc9dc6953743079f7
jackmccabe-elsewhen <66681105+j-mccabe@users.noreply.github.com>
2023-12-08 09:11:55 +0000

    separate out jwt function

--- app/auth/authenticate.js
+++ app/auth/authenticate.js
@@ -3,23 +3,25 @@ import jwksClient from 'jwks-rsa'
 
 import logger from '../utils/logger.js'
 
+export async function getJwtPublicKey (kid) {
+  const client = jwksClient({
+    jwksUri: `https://login.microsoftonline.com/${process.env.API_TENANT_ID}/discovery/v2.0/keys`
+  })
+  const key = await client.getSigningKey(kid)
+  return key.getPublicKey()
+}
+
 export async function getAuth (request) {
   try {
     const authHeader = request?.headers?.authorization
     if (!authHeader) {
       return {}
     }
-    const client = jwksClient({
-      jwksUri: `https://login.microsoftonline.com/${process.env.API_TENANT_ID}/discovery/v2.0/keys`
-    })
     const token = authHeader.split(' ')[1]
     const decodedToken = jwt.decode(token, { complete: true })
     const header = decodedToken.header
-    const key = await client.getSigningKey(header.kid)
-    const signingKey = key.getPublicKey()
-    const decoded = jwt.verify(token, signingKey)
-
-    return decoded
+    const signingKey = await getJwtPublicKey(header.kid)
+    return jwt.verify(token, signingKey)
   } catch (error) {
     logger.error('#authenticate - Error verifying jwt', { error })
     return {}
