3adfc862edc7ab489efb86b8530e478425341602
jackmccabe-elsewhen <66681105+j-mccabe@users.noreply.github.com>
2023-12-08 09:13:56 +0000

    fix http status

--- app/errors/graphql.js
+++ app/errors/graphql.js
@@ -3,6 +3,6 @@ import { GraphQLError } from 'graphql'
 export class Unauthorized extends GraphQLError {
   constructor (opts) {
     super(opts)
-    this.extensions = { code: 'UNAUTHORIZED', http: { status: 404 } }
+    this.extensions = { code: 'UNAUTHORIZED', http: { status: 401 } }
   }
 }
