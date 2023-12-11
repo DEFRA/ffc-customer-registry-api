3a2cac80ac24c1e9d0baf1006b4001b864e5ad3e
Hadi <hadi.tajallaei@defra.gov.uk>
2023-12-11 10:41:22 +0000

    ensured async test in encapusated inside it or test as per jest convention

--- test/integration/graphql/business.test.js
+++ test/integration/graphql/business.test.js
@@ -104,75 +104,77 @@ describe('Query.customer', () => {
   })
 })
 
-describe('Query businessApplications', async () => {
-  const result = await graphql({
-    source: `#graphql
-    query BusinessApplications {
-        businessApplications(id: "5444918") {
-            applicationStatus {
-                id
-                open
-                status
-                type
-                sector
-                year
-                frn
-                office
-            }
-            csClaim {
-                schemeYear
-                type
-                status
-                lastMovement
-            }
-        }
-    }
-    `,
-    variableValues: {
-      customerId: '5444918'
-    },
-    schema,
-    contextValue: fakeContext
-  })
-
-  expect(result).toEqual({
-    data: [
-      {
-        applicationStatus: {
-          id: expect.any(Number),
-          open: null,
-          status: 'Withdrawn',
-          type: expect.any(String),
-          sector: null,
-          year: expect.any(Number),
-          frn: expect.any(Number),
-          office: null
-        },
-        csClaim: {
-          schemaYear: expect.any(Number),
-          type: expect.any(String),
-          status: 'WTHDRW',
-          lastMovement: expect.any(String)
-        }
-      },
-      {
-        applicationStatus: {
-          id: expect.any(Number),
-          open: null,
-          status: 'Checking Application',
-          type: expect.any(String),
-          sector: 'STANDA',
-          year: expect.any(Number),
-          frn: expect.any(Number),
-          office: null
-        },
-        csClaim: {
-          schemaYear: expect.any(Number),
-          type: expect.any(String),
-          status: 'AGROFF',
-          lastMovement: expect.any(String)
-        }
+describe('Query businessApplications', () => {
+  it('should bring expected data back', async () => {
+    const result = await graphql({
+      source: `#graphql
+      query BusinessApplications {
+          businessApplications(id: "5444918") {
+              applicationStatus {
+                  id
+                  open
+                  status
+                  type
+                  sector
+                  year
+                  frn
+                  office
+              }
+              csClaim {
+                  schemeYear
+                  type
+                  status
+                  lastMovement
+              }
+          }
       }
-    ]
+      `,
+      variableValues: {
+        customerId: '5444918'
+      },
+      schema,
+      contextValue: fakeContext
+    })
+
+    expect(result).toEqual({
+      data: [
+        {
+          applicationStatus: {
+            id: expect.any(Number),
+            open: null,
+            status: 'Withdrawn',
+            type: expect.any(String),
+            sector: null,
+            year: expect.any(Number),
+            frn: expect.any(Number),
+            office: null
+          },
+          csClaim: {
+            schemaYear: expect.any(Number),
+            type: expect.any(String),
+            status: 'WTHDRW',
+            lastMovement: expect.any(String)
+          }
+        },
+        {
+          applicationStatus: {
+            id: expect.any(Number),
+            open: null,
+            status: 'Checking Application',
+            type: expect.any(String),
+            sector: 'STANDA',
+            year: expect.any(Number),
+            frn: expect.any(Number),
+            office: null
+          },
+          csClaim: {
+            schemaYear: expect.any(Number),
+            type: expect.any(String),
+            status: 'AGROFF',
+            lastMovement: expect.any(String)
+          }
+        }
+      ]
+    })
   })
 })
