ee42e4af338fd4d5bfdeab2062cdf2a73cf8dbda
Hadi <hadi.tajallaei@defra.gov.uk>
2023-12-28 14:02:31 +0000

    changed parameters order

--- app/graphql/resolvers/business/cph/query.js
+++ app/graphql/resolvers/business/cph/query.js
@@ -4,8 +4,8 @@ export const CPH = async (_, { id }, { dataSources }) => {
   return await dataSources.ruralPaymentsPortalApi.getOrganisationCPHCollectionBySBI(id)
 }
 
-export const CPHInfo = async ({ business, cphNumber }, _, { dataSources }) => {
-  const response = await dataSources.ruralPaymentsPortalApi.getOrganisationCPHInfoBySBIAndCPHNumber(business.id, cphNumber)
+export const CPHInfo = async ({ cphNumber }, { id }, { dataSources }) => {
+  const response = await dataSources.ruralPaymentsPortalApi.getOrganisationCPHInfoBySBIAndCPHNumber(id, cphNumber)
 
   return transformOrganisationCPHInfo(response)
 }
