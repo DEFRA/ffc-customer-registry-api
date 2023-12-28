8a8560deda3ccb02223b260861e5dc415c3163f4
Hadi <hadi.tajallaei@defra.gov.uk>
2023-12-28 12:27:39 +0000

    corrected parameter parsed

--- app/graphql/resolvers/business/query.js
+++ app/graphql/resolvers/business/query.js
@@ -24,12 +24,12 @@ export const Query = {
   }
 }
 
-export const CPH = async ({ id }, _, { dataSources }) => {
+export const CPH = async (_, { id }, { dataSources }) => {
   return await dataSources.ruralPaymentsPortalApi.getOrganisationCPHCollectionBySBI(id)
 }
 
-export const CPHInfo = async ({ id, cphNumber }, _, { dataSources }) => {
-  const response = await dataSources.ruralPaymentsPortalApi.getOrganisationCPHInfoBySBIAndCPHNumber(id, cphNumber)
+export const CPHInfo = async ({ business, cphNumber }, _, { dataSources }) => {
+  const response = await dataSources.ruralPaymentsPortalApi.getOrganisationCPHInfoBySBIAndCPHNumber(business.id, cphNumber)
 
   return transformOrganisationCPHInfo(response)
 }
