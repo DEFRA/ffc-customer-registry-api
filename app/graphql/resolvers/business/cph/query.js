71c6428a71f69235e83169a078dac6f880790d82
Hadi <hadi.tajallaei@defra.gov.uk>
2024-01-04 13:51:58 +0000

    attempt to fix issue with type of resolvr definiton

--- app/graphql/resolvers/business/cph/query.js
+++ app/graphql/resolvers/business/cph/query.js
@@ -1,16 +1,16 @@
 import { transformOrganisationCPH, transformOrganisationCPHInfo } from '../../../../transformers/rural-payments-portal/business-cph.js'
 
-export const CPH = async (_, { id }, { dataSources }) => {
+export const CPH = (async (_, { id }, { dataSources }) => {
   const response = await dataSources.ruralPaymentsPortalApi.getOrganisationCPHCollectionBySBI(id)
 
   return {
     ...transformOrganisationCPH(response),
     info: CPHInfo
   }
-}
+})
 
-export const CPHInfo = async ({ cphNumber }, { id }, { dataSources }) => {
+export const CPHInfo = (async ({ cphNumber }, { id }, { dataSources }) => {
   const response = await dataSources.ruralPaymentsPortalApi.getOrganisationCPHInfoBySBIAndCPHNumber(id, cphNumber)
 
   return transformOrganisationCPHInfo(response)
-}
+})
