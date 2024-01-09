cdabd53fa975764b7be1d57507fbec814c0d872e
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2024-01-09 10:49:32 +0000

    move code from transformer to resolver

--- app/graphql/resolvers/business/business.js
+++ app/graphql/resolvers/business/business.js
@@ -12,3 +12,63 @@ export const Business = {
     )
   }
 }
+
+export const CPH = {
+  async parish ({ id, number }, _, { dataSources }) {
+    const response = await dataSources
+      .ruralPaymentsPortalApi
+      .getOrganisationCPHInfoBySBIAndCPHNumber(
+        id,
+        number
+      )
+
+    return response.parish
+  },
+
+  async startDate ({ id, number }, __, { dataSources }) {
+    const response = await dataSources
+      .ruralPaymentsPortalApi
+      .getOrganisationCPHInfoBySBIAndCPHNumber(
+        id,
+        number
+      )
+
+    return response.startDate
+  },
+
+  async expiryDate ({ id, number }, __, { dataSources }) {
+    const response = await dataSources
+      .ruralPaymentsPortalApi
+      .getOrganisationCPHInfoBySBIAndCPHNumber(
+        id,
+        number
+      )
+
+    return response.expiryDate
+  },
+
+  async species ({ id, number }, __, { dataSources }) {
+    const response = await dataSources
+      .ruralPaymentsPortalApi
+      .getOrganisationCPHInfoBySBIAndCPHNumber(
+        id,
+        number
+      )
+
+    return response.species
+  },
+
+  async coordinate ({ id, number }, __, { dataSources }) {
+    const response = await dataSources
+      .ruralPaymentsPortalApi
+      .getOrganisationCPHInfoBySBIAndCPHNumber(
+        id,
+        number
+      )
+
+    return {
+      x: response.xCoordinate,
+      y: response.yCoordinate
+    }
+  }
+}
