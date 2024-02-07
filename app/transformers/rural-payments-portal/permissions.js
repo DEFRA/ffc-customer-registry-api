95ee0a4b5bcec772062b19449189f0827a64a5db
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2024-02-07 12:04:00 +0000

    fix lint error

--- app/transformers/rural-payments-portal/permissions.js
+++ app/transformers/rural-payments-portal/permissions.js
@@ -1,6 +1,6 @@
 export function transformOrganisationAuthorisationToCustomerBusinessPermissionLevel (permissions, authorisation) {
   permissions.reverse()
-  const privilegeNames = authorisation.flatMap(({ privilegeNames }) => privilegeNames)
+  const privilegeNames = authorisation.flatMap((personPrivileges) => personPrivileges.privilegeNames)
 
   for (const permission of permissions) {
     if (privilegeNames.some(privilege => permission.privilegeNames.includes(privilege))) {
